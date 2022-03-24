e5740d2ce7b33099a1eaa0eded2c1e25247c21cb
mr-chidex <mbahchiboy2@gmail.com>
2022-03-24 18:13:12 +0100

    image single page added

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,14 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Image, Text, ScrollView } from "react-native";
+import api from "../utils/api";
 
-import { NavProps } from "../utils/types";
+import { NavProps, Photo } from "../utils/types";
 
 type Props = NavProps;
 
 const Details: React.FC<Props> = ({ navigation }) => {
-  const ID = navigation.state.params?.itemId as string | undefined;
+  const [photo, setPhoto] = useState<Photo>();
+  const [errorMessage, setErrorMessage] = useState("");
+  const ID = navigation.getParam("itemId") as string;
 
-  console.log(navigation);
-  return <div>Details - {ID}</div>;
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get(`/images/${ID}`);
+        setPhoto(data);
+      } catch (error: any) {
+        setErrorMessage(error.message);
+      }
+    })();
+  }, []);
+
+  return (
+    <ScrollView style={styles.container}>
+      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
+
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: photo?.url }} />
+      </View>
+      <Text style={styles.itemName}>{photo?.name}</Text>
+
+      <Text style={{ marginVertical: 10, fontSize: 18 }}>Uploaded by:</Text>
+
+      <Image style={styles.authorImage} source={{ uri: photo?.author.image }} />
+
+      <Text style={styles.author}>
+        Author: {`${photo?.author.firstname} ${photo?.author.lastname}`}
+      </Text>
+      <Text style={styles.author}>Email: {photo?.author.email}</Text>
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 10,
+    marginVertical: 15,
+    flex: 1,
+  },
+  imageContainer: {
+    height: 320,
+    backgroundColor: "#ccc",
+    marginBottom: 10,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  itemName: {
+    textAlign: "center",
+    fontWeight: "500",
+    fontSize: 17,
+  },
+  authorImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  author: {
+    marginVertical: 10,
+    fontWeight: "bold",
+    fontSize: 17,
+    fontStyle: "italic",
+  },
+  error: {
+    textAlign: "center",
+    color: "red",
+  },
+});
 
 export default Details;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,9 +8,21 @@ export type Photos = {
   _id: number;
   name: string;
   url: string;
-  author: string;
 };
 
 export interface NavProps {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
+}
+
+export interface Photo {
+  _id: string;
+  name: string;
+  url: string;
+  author: {
+    email: string;
+    firstname: string;
+    lastname: string;
+    image: string;
+  };
+  createdAt: Date;
 }
